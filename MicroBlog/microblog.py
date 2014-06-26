527af2868d29405f37db265279506dbf9d7b5ece
Victoria de Winter <dewinter.victoria@gmail.com>
2014-06-25 22:34:30 -0700

    Finished tutorial

--- MicroBlog/microblog.py
+++ MicroBlog/microblog.py
@@ -55,9 +55,8 @@ def show_entries():
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    db.get_db()
-    db.execute('INSERT INTO entries (title, text VALUES (?, ?'),
-                [request.form['title'], request.form['text']])
+    db = get_db()
+    db.execute('INSERT INTO entries (title, text) VALUES (?, ?)', [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
